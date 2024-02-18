7c11e885a43bdc65adc0f6278493b3df6db55eac
sepa38 <82813488+sepa38@users.noreply.github.com>
2024-02-18 21:08:14 +0900

    style: correction of naming rule

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import discord
 
 
 RAISED_HAND = "\N{Raised Hand}"
-participationMessage = None
+participation_message = None
 
 try:
     with open("TOKEN.txt", mode = "r") as f:
@@ -15,15 +15,15 @@ except:
     exit()
 
 try:
-    with open("ONGOING.txt", mode = "r") as f:
-        isOngoing = int(f.read())
+    with open("ongoing.txt", mode = "r") as f:
+        is_ongoing = int(f.read())
 except:
-    isOngoing = 0
-    with open("ONGOING.txt", mode = "w") as f:
-        f.write(str(isOngoing))
+    is_ongoing = 0
+    with open("ongoing.txt", mode = "w") as f:
+        f.write(str(is_ongoing))
 
 try:
-    with open("HOME_CHANNEL_ID.txt", mode = "r") as f:
+    with open("home_channel_id.txt", mode = "r") as f:
         HOME_CHANNEL_ID = int(f.read())
 except:
     HOME_CHANNEL_ID = None
@@ -40,7 +40,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     # HACK: 後でクラスにまとめる
-    global HOME_CHANNEL_ID, isOngoing, participationMessage, participant_list, individualChannelList, currentTurn, completedUsers
+    global HOME_CHANNEL_ID, is_ongoing, participation_message, participant_list, individual_channel_list, current_turn, completed_users
 
     if not message.content.startswith("!"):
         return
@@ -48,7 +48,7 @@ async def on_message(message):
     elif message.content.startswith("!set_home_channel"):
         HOME_CHANNEL_ID = message.channel.id
 
-        with open("HOME_CHANNEL_ID.txt", mode = "w") as f:
+        with open("home_channel_id.txt", mode = "w") as f:
             f.write(str(HOME_CHANNEL_ID))
 
         await message.channel.send("このチャンネルを 1WGP のホームチャンネルに設定しました")
@@ -57,108 +57,108 @@ async def on_message(message):
         await message.channel.send("```\n!set_home_channel\n```でホームチャンネルを設定してください")
 
     elif message.content.startswith("!new_game"):
-        if isOngoing:
+        if is_ongoing:
             await message.channel.send("現在進行中のゲームがあるようです。新しくゲームを始める場合、 \
                 ```\n!cancel_game\n```\nを用いて進行中のゲームを中断してください")
         else:
-            participationMessage = await message.channel.send(f"参加希望者はこのメッセージに {RAISED_HAND} のリアクションをつけてください")
-            await participationMessage.add_reaction(RAISED_HAND)
+            participation_message = await message.channel.send(f"参加希望者はこのメッセージに {RAISED_HAND} のリアクションをつけてください")
+            await participation_message.add_reaction(RAISED_HAND)
             participant_list = []
-            individualChannelList = []
-            completedUsers = set()
+            individual_channel_list = []
+            completed_users = set()
 
-            isOngoing = 1
-            with open("ONGOING.txt", mode = "w") as f:
-                f.write(str(isOngoing))
+            is_ongoing = 1
+            with open("ongoing.txt", mode = "w") as f:
+                f.write(str(is_ongoing))
 
     elif message.content.startswith("!confirm"):
-        if isOngoing:
+        if is_ongoing:
             participant_mention_list = [participant_i.mention for participant_i in participant_list]
             confirm_message = "以下のメンバーでゲームを開始します\n" + "\n".join(participant_mention_list)
             await message.channel.send(confirm_message)
             
-            participant_ID_list = [str(participant_i.id) for participant_i in participant_list]
+            participant_id_list = [str(participant_i.id) for participant_i in participant_list]
             with open("participants_ID_list.txt", mode = "w") as f:
-                f.write(" ".join(participant_ID_list))
+                f.write(" ".join(participant_id_list))
 
             startDate = str(datetime.datetime.now().replace(microsecond=0))
             startDate = startDate.replace(" ", "_").replace(":", "-")
             os.mkdir(startDate)
 
-            numberOfParticipants = len(participant_list)
-            for turn in range(numberOfParticipants):
-                for user_index in range(numberOfParticipants):
+            number_of_participants = len(participant_list)
+            for turn in range(number_of_participants):
+                for user_index in range(number_of_participants):
                     os.makedirs(f"{startDate}/{turn}/{user_index}")
 
-            passingTable = [[] for i in range(numberOfParticipants)]
-            for turn in range(numberOfParticipants):
-                for user_index in range(numberOfParticipants):
-                    user_tmp = participant_list[(turn+user_index)%numberOfParticipants]
-                    passingTable[turn].append(user_tmp)
-            random.shuffle(passingTable)
+            passing_table = [[] for i in range(number_of_participants)]
+            for turn in range(number_of_participants):
+                for user_index in range(number_of_participants):
+                    user_tmp = participant_list[(turn+user_index)%number_of_participants]
+                    passing_table[turn].append(user_tmp)
+            random.shuffle(passing_table)
 
-            passingTableForSave = []
-            for turn in range(numberOfParticipants):
-                turnUsers = [str(user_tmp.id) for user_tmp in passingTable[turn]]
-                passingTableForSave.append(" ".join(turnUsers))
+            passing_table_for_save = []
+            for turn in range(number_of_participants):
+                turnUsers = [str(user_tmp.id) for user_tmp in passing_table[turn]]
+                passing_table_for_save.append(" ".join(turnUsers))
             with open("passing_table.txt", mode = "w") as f:
-                f.write("\n".join(passingTableForSave))
+                f.write("\n".join(passing_table_for_save))
 
         else:
             await message.channel.send("```\n!new_game\n```\nを用いてゲーム参加者を募集してください")
 
     elif message.content.startswith("!cancel_game"):
-        isOngoing = 0
-        with open("ONGOING.txt", mode = "w") as f:
-            f.write(str(isOngoing))
+        is_ongoing = 0
+        with open("ongoing.txt", mode = "w") as f:
+            f.write(str(is_ongoing))
 
         await message.channel.send("現在進行中のゲームを中断しました")
 
     elif message.content.startswith("!send_subject"):
-        if message.channel not in individualChannelList:
+        if message.channel not in individual_channel_list:
             return
 
-        if not isOngoing:
+        if not is_ongoing:
             await message.channel.send("現在進行中のゲームがありません")
             return
 
-        if currentTurn % 2 == 1:
+        if current_turn % 2 == 1:
             await message.channel.send("現在、絵を送信するフェーズです。\n```\n!send_picture\n```\nを用いて画像を送信してください")
             return
         
-        userIndex = participant_ID_list.index(message.author.id)
-        targetPath = os.path.join(startDate, str(currentTurn))
-        targetPath = os.path.join(targetPath, str(userIndex))
-        with open(os.path.join(targetPath, "subject.txt")) as f:
+        user_index = participant_id_list.index(message.author.id)
+        target_path = os.path.join(startDate, str(current_turn))
+        target_path = os.path.join(target_path, str(user_index))
+        with open(os.path.join(target_path, "subject.txt")) as f:
             f.write(message.content) # FIXME: !send_subject ごと書き込まれる
 
-        completedUsers.add(message.author.id)
-        if len(completedUsers) == numberOfParticipants:
+        completed_users.add(message.author.id)
+        if len(completed_users) == number_of_participants:
             pass
             # next_job() TODO: send_picture とまとめる
 
     elif message.content.startswith('!send_picture'):
-        if message.channel not in individualChannelList:
+        if message.channel not in individual_channel_list:
             return
 
-        if not isOngoing:
+        if not is_ongoing:
             await message.channel.send("現在進行中のゲームがありません")
             return
 
-        if currentTurn % 2 == 0:
+        if current_turn % 2 == 0:
             await message.channel.send("現在、お題もしくは絵の説明を送信するフェーズです。\n```\n!send_subject\n```\nを用いて文章を送信してください")
             return
 
-        userIndex = participant_ID_list.index(message.author.id)
-        targetPath = os.path.join(startDate, str(currentTurn))
-        targetPath = os.path.join(targetPath, str(userIndex))
+        user_index = participant_id_list.index(message.author.id)
+        target_path = os.path.join(startDate, str(current_turn))
+        target_path = os.path.join(target_path, str(user_index))
 
         attachment = message.attachments[0]
-        fileName = os.path.join(targetPath, attachment.filename)
-        await attachment.save(fileName)
+        file_name = os.path.join(target_path, attachment.filename)
+        await attachment.save(file_name)
         
-        completedUsers.add(message.author.id)
-        if len(completedUsers) == numberOfParticipants:
+        completed_users.add(message.author.id)
+        if len(completed_users) == number_of_participants:
             pass
             # next_job() TODO: send_subject とまとめる
 
@@ -168,16 +168,16 @@ async def on_message(message):
 
 @client.event
 async def on_reaction_add(reaction, user):
-    global participationMessage, participant_list, individualChannelList
+    global participation_message, participant_list, individual_channel_list
 
     message = reaction.message
     category = message.channel.category
     channel_name = user.name
 
-    if participationMessage is None:
+    if participation_message is None:
         return
 
-    if message != participationMessage or user == client.user:
+    if message != participation_message or user == client.user:
         return
 
     participant_list.append(user)
@@ -185,7 +185,7 @@ async def on_reaction_add(reaction, user):
     # 目的のチャンネルがすでに作られているとき
     for channel_i in category.text_channels:
         if channel_name == channel_i.name:
-            individualChannelList.append(channel_i)
+            individual_channel_list.append(channel_i)
             return
 
     # プライベートチャンネルに設定するための dict
@@ -196,21 +196,21 @@ async def on_reaction_add(reaction, user):
 
     channel = await category.create_text_channel(name=channel_name, overwrites=permission)
 
-    individualChannelList.append(channel)
+    individual_channel_list.append(channel)
 
     await channel.send("このチャンネルで画像、テキストの送受信をしてください")
 
 
 @client.event
 async def on_reaction_remove(reaction, user):
-    global participationMessage, participant_list
+    global participation_message, participant_list
 
     message = reaction.message
 
-    if participationMessage is None:
+    if participation_message is None:
         return
 
-    if message != participationMessage or user == client.user:
+    if message != participation_message or user == client.user:
         return
 
     participant_list.remove(user)
